7a389637f4f7b5ad16fa98ffaf53ecac05271922
VamanPatel <vamankumar456@gmail.com>
2021-11-14 19:10:34 +0530

    Done todays task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import {
+  DataStateChangeEvent,
+  GridDataResult,
+  PageChangeEvent,
+} from '@progress/kendo-angular-grid';
+import {
+  State,
+  process,
+  SortDescriptor,
+  orderBy,
+} from '@progress/kendo-data-query';
+import { products } from './product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'kendo-demo-task';
+
+  public items: any[] = products;
+  public mySelection: number[] = [];
+  public pageSize = 20;
+  public skip = 0;
+
+  public allowUnsort = true;
+
+  public buttonCount = 5;
+  public info = true;
+  public pageSizes = true;
+  public previousNext = true;
+  public position: any = 'bottom';
+  public type: any = 'numeric';
+
+  constructor() {}
+
+  public pageChange(event: PageChangeEvent): void {
+    this.skip = event.skip;
+
+    // Optionally, clear the selection when paging
+    // this.mySelection = [];
+  }
+
+  cellClickHandler(event: any) {
+    console.log(event.dataItem);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  ExcelModule,
+  GridModule,
+  PDFModule,
+} from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GridModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    PDFModule,
+    ExcelModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
